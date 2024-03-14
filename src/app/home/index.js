f1fdba7f2fe31905e3a41fbfb8ab951880b63a22
Yash babariya <162944706+yash-babariya@users.noreply.github.com>
2024-03-14 14:33:02 +0000

    some changes...

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Profile from './profile'
 import AboutMe from './aboutMe'
+import Skill from './skill'
 
 export default function Home() {
     return (
         <div>
             <Profile />
             <AboutMe />
+            <Skill />
         </div>
     )
 }
---
+++ src/app/home/skill/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function Skill() {
+    return (
+        <div className='skill'>
+            <div className="container">
+                <section>
+                    <div className="heading">
+                        <h2>MY Skills</h2>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
